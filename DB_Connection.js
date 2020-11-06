799f72247586eb74ef92e7cec2e120746ce0a8f1
Your Name <phamphong3699@gmail.com>
2020-11-06 16:00:47 +0700

    fix connection

--- DB_Connection.js
+++ DB_Connection.js
@@ -2,7 +2,7 @@ const mongodb = require('mongoose');
 require('dotenv/config');
 
 const dbconnection = () => {
-    const connection = mongodb.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    const connection = mongodb.connect("mongodb+srv://PhongBlog:phongpro99@cluster0.hozns.mongodb.net/PhongBlog?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
         if (err) { console.log(err) } else { console.log('DB CONNECTION OK') };
     });
     return connection;
